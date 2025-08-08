f1a6605958144e004dd8f25e9b3d0dc8a4ad980d
KIJESHOES <142238267+KIJESHOES@users.noreply.github.com>
2025-08-08 10:07:44 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
 });
+
+Route::get('/kontak-kami', fn() => view('kontak-kami'));
+Route::get('/kosmetik', fn() => view('kosmetik'));
+Route::get('/materi-fkhp-2025', fn() => view('materi-fkhp-2025'));
+Route::get('/obat-tradisional', fn() => view('obat-tradisional'));
+Route::get('/obat', fn() => view('obat'));
+Route::get('/pangan', fn() => view('pangan'));
+Route::get('/suplamen-kesehatan', fn() => view('suplamen-kesehatan'));
+Route::get('/single', fn() => view('single'));
